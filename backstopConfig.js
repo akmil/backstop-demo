ba2333391c029fcfc80eadf0fece59e7111a48b2
Pavlo Oliinyk <Pavlo.Oliinyk@wolterskluwer.com>
2021-08-13 18:23:56 +0300

    upd

--- backstopConfig.js
+++ backstopConfig.js
@@ -2,19 +2,45 @@ module.exports = {
     id: "uniq_id_config",
     viewports: [
         {
+            label: "mobile",
+            width: 380,
+            height: 640
+        },
+        {
             label: "tablet",
             width: 1024,
             height: 768
+        },
+        {
+            label: "desktop",
+            width: 1920,
+            height: 1024
         }
     ],
     onBeforeScript: "puppet/onBefore.js",
     onReadyScript: "puppet/onReady.js",
     scenarios: [
         {
-            label: 'Home',
+            label: 'Home_en',
             misMatchThreshold : 0.5,
-            referenceUrl: 'https://dewalt.com/',
-            url: 'https://dewalt.com/'}
+            referenceUrl: 'https://www.wolterskluwer.com/en/kitchen-sinks/2dot0/a-z-solution',
+            url: 'https://www.wolterskluwer.com/en/kitchen-sinks/2dot0/a-z-solution',
+            cookiePath: 'cookies.json'
+        },
+        {
+            label: 'Home_nl',
+            misMatchThreshold : 0.5,
+            referenceUrl: 'https://www.wolterskluwer.com/nl-nl/kitchen-sinks/2dot0/a-z-solution',
+            url: 'https://www.wolterskluwer.com/nl-nl/kitchen-sinks/2dot0/a-z-solution',
+            cookiePath: 'cookies.json'
+        },
+        {
+            label: 'Home_de',
+            misMatchThreshold : 0.5,
+            referenceUrl: 'https://www.wolterskluwer.com/de-de/kitchen-sinks/2dot0/a-z-solution',
+            url: 'https://www.wolterskluwer.com/de-de/kitchen-sinks/2dot0/a-z-solution',
+            cookiePath: 'cookies.json'
+        }
         ],
     paths: {
         bitmaps_reference: "backstop_data/bitmaps_reference",
--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -3,14 +3,37 @@ report({
   "tests": [
     {
       "pair": {
-        "reference": "..\\bitmaps_reference\\uniq_id_config_Home_0_document_0_tablet.png",
-        "test": "..\\bitmaps_test\\20210726-185058\\uniq_id_config_Home_0_document_0_tablet.png",
+        "reference": "..\\bitmaps_reference\\uniq_id_config_Home_en_0_document_0_mobile.png",
+        "test": "..\\bitmaps_test\\20210813-122039\\uniq_id_config_Home_en_0_document_0_mobile.png",
         "selector": "document",
-        "fileName": "uniq_id_config_Home_0_document_0_tablet.png",
-        "label": "Home",
+        "fileName": "uniq_id_config_Home_en_0_document_0_mobile.png",
+        "label": "Home_en",
         "misMatchThreshold": 0.5,
-        "url": "https://dewalt.com/",
-        "referenceUrl": "https://dewalt.com/",
+        "url": "https://www.wolterskluwer.com/en/kitchen-sinks/2dot0/a-z-solution",
+        "referenceUrl": "https://www.wolterskluwer.com/en/kitchen-sinks/2dot0/a-z-solution",
+        "expect": 0,
+        "viewportLabel": "mobile",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\uniq_id_config_Home_en_0_document_1_tablet.png",
+        "test": "..\\bitmaps_test\\20210813-122039\\uniq_id_config_Home_en_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "uniq_id_config_Home_en_0_document_1_tablet.png",
+        "label": "Home_en",
+        "misMatchThreshold": 0.5,
+        "url": "https://www.wolterskluwer.com/en/kitchen-sinks/2dot0/a-z-solution",
+        "referenceUrl": "https://www.wolterskluwer.com/en/kitchen-sinks/2dot0/a-z-solution",
         "expect": 0,
         "viewportLabel": "tablet",
         "diff": {
@@ -19,12 +42,181 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "21.83",
-          "analysisTime": 47
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\uniq_id_config_Home_en_0_document_2_desktop.png",
+        "test": "..\\bitmaps_test\\20210813-122039\\uniq_id_config_Home_en_0_document_2_desktop.png",
+        "selector": "document",
+        "fileName": "uniq_id_config_Home_en_0_document_2_desktop.png",
+        "label": "Home_en",
+        "misMatchThreshold": 0.5,
+        "url": "https://www.wolterskluwer.com/en/kitchen-sinks/2dot0/a-z-solution",
+        "referenceUrl": "https://www.wolterskluwer.com/en/kitchen-sinks/2dot0/a-z-solution",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\uniq_id_config_Home_nl_0_document_0_mobile.png",
+        "test": "..\\bitmaps_test\\20210813-122039\\uniq_id_config_Home_nl_0_document_0_mobile.png",
+        "selector": "document",
+        "fileName": "uniq_id_config_Home_nl_0_document_0_mobile.png",
+        "label": "Home_nl",
+        "misMatchThreshold": 0.5,
+        "url": "https://www.wolterskluwer.com/nl-nl/kitchen-sinks/2dot0/a-z-solution",
+        "referenceUrl": "https://www.wolterskluwer.com/nl-nl/kitchen-sinks/2dot0/a-z-solution",
+        "expect": 0,
+        "viewportLabel": "mobile",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "19.00",
+          "analysisTime": 397
         },
-        "diffImage": "..\\bitmaps_test\\20210726-185058\\failed_diff_uniq_id_config_Home_0_document_0_tablet.png"
+        "diffImage": "..\\bitmaps_test\\20210813-122039\\failed_diff_uniq_id_config_Home_nl_0_document_0_mobile.png"
       },
       "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\uniq_id_config_Home_nl_0_document_1_tablet.png",
+        "test": "..\\bitmaps_test\\20210813-122039\\uniq_id_config_Home_nl_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "uniq_id_config_Home_nl_0_document_1_tablet.png",
+        "label": "Home_nl",
+        "misMatchThreshold": 0.5,
+        "url": "https://www.wolterskluwer.com/nl-nl/kitchen-sinks/2dot0/a-z-solution",
+        "referenceUrl": "https://www.wolterskluwer.com/nl-nl/kitchen-sinks/2dot0/a-z-solution",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.11",
+          "analysisTime": 21279
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\uniq_id_config_Home_nl_0_document_2_desktop.png",
+        "test": "..\\bitmaps_test\\20210813-122039\\uniq_id_config_Home_nl_0_document_2_desktop.png",
+        "selector": "document",
+        "fileName": "uniq_id_config_Home_nl_0_document_2_desktop.png",
+        "label": "Home_nl",
+        "misMatchThreshold": 0.5,
+        "url": "https://www.wolterskluwer.com/nl-nl/kitchen-sinks/2dot0/a-z-solution",
+        "referenceUrl": "https://www.wolterskluwer.com/nl-nl/kitchen-sinks/2dot0/a-z-solution",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -1036
+          },
+          "misMatchPercentage": "1.11",
+          "analysisTime": 28408
+        },
+        "diffImage": "..\\bitmaps_test\\20210813-122039\\failed_diff_uniq_id_config_Home_nl_0_document_2_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\uniq_id_config_Home_de_0_document_0_mobile.png",
+        "test": "..\\bitmaps_test\\20210813-122039\\uniq_id_config_Home_de_0_document_0_mobile.png",
+        "selector": "document",
+        "fileName": "uniq_id_config_Home_de_0_document_0_mobile.png",
+        "label": "Home_de",
+        "misMatchThreshold": 0.5,
+        "url": "https://www.wolterskluwer.com/de-de/kitchen-sinks/2dot0/a-z-solution",
+        "referenceUrl": "https://www.wolterskluwer.com/de-de/kitchen-sinks/2dot0/a-z-solution",
+        "expect": 0,
+        "viewportLabel": "mobile",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "14.04",
+          "analysisTime": 375
+        },
+        "diffImage": "..\\bitmaps_test\\20210813-122039\\failed_diff_uniq_id_config_Home_de_0_document_0_mobile.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\uniq_id_config_Home_de_0_document_1_tablet.png",
+        "test": "..\\bitmaps_test\\20210813-122039\\uniq_id_config_Home_de_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "uniq_id_config_Home_de_0_document_1_tablet.png",
+        "label": "Home_de",
+        "misMatchThreshold": 0.5,
+        "url": "https://www.wolterskluwer.com/de-de/kitchen-sinks/2dot0/a-z-solution",
+        "referenceUrl": "https://www.wolterskluwer.com/de-de/kitchen-sinks/2dot0/a-z-solution",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "2.52",
+          "analysisTime": 23155
+        },
+        "diffImage": "..\\bitmaps_test\\20210813-122039\\failed_diff_uniq_id_config_Home_de_0_document_1_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\uniq_id_config_Home_de_0_document_2_desktop.png",
+        "test": "..\\bitmaps_test\\20210813-122039\\uniq_id_config_Home_de_0_document_2_desktop.png",
+        "selector": "document",
+        "fileName": "uniq_id_config_Home_de_0_document_2_desktop.png",
+        "label": "Home_de",
+        "misMatchThreshold": 0.5,
+        "url": "https://www.wolterskluwer.com/de-de/kitchen-sinks/2dot0/a-z-solution",
+        "referenceUrl": "https://www.wolterskluwer.com/de-de/kitchen-sinks/2dot0/a-z-solution",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.01",
+          "analysisTime": 28449
+        }
+      },
+      "status": "pass"
     }
   ],
   "id": "uniq_id_config"
